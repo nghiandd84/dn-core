ac64f16e33da6bda6b48d2de4e63bec05b9a60d0
NGHIA Nguyen Duc Dai <nghia.nguyenduc@eightcap.com>
2022-01-13 16:39:23 +0700

    App Port

--- src/constant/AppConstant.ts
+++ src/constant/AppConstant.ts
@@ -1,6 +1,12 @@
 export enum APP_ID {
-    USER = 'DN-USER',
-    LANGUAGE = 'DN-LANGUAGE',
-    NOTIFICATION = 'DN-NOTIFICATION',
+    USER = 'dn_user',
+    LANGUAGE = 'dn_language',
+    NOTIFICATION = 'dn_language',
     
+}
+
+export enum APP_PORT {
+    USER = 18001,
+    LANGUAGE = 18002,
+    NOTIFICATION = 18003
 }
